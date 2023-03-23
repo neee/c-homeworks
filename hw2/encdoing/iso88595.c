340784dd7ed205525281c7faa33e67f14259fcb6
Sergey Serdyuk <sergey.serdyuk@agoraworld.co>
2023-03-23 23:30:10 +0300

    hw2: fix numbers

--- hw2/encdoing/iso88595.c
+++ hw2/encdoing/iso88595.c
@@ -9,11 +9,11 @@ void convert_from_iso88595_to_utf8(FILE *input_file, FILE *output_file) {
     unsigned char utf8_bytes[2];
 
     while (fread(&iso_8859_5_byte, 1, 1, input_file) > 0) {
-        if (iso_8859_5_byte >= 128 && iso_8859_5_byte < 224) {
+        if (iso_8859_5_byte >= 0x80 && iso_8859_5_byte < 0xE0) {
             utf8_bytes[0] = 0xD0;
             utf8_bytes[1] = iso_8859_5_byte - 0x20;
             fwrite(utf8_bytes, 1, 2, output_file);
-        } else if (iso_8859_5_byte >= 224) {
+        } else if (iso_8859_5_byte >= 0xE0) {
             utf8_bytes[0] = 0xD1;
             utf8_bytes[1] = iso_8859_5_byte - 0x60;
             fwrite(utf8_bytes, 1, 2, output_file);
