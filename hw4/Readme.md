ad4e407d33f806f3bb3a672cf81d6b55419101fe
Sergey Serdyuk <sergey.serdyuk@agoraworld.co>
2023-04-02 16:11:35 +0300

    hw4: Add readme, read http get

---
+++ hw4/Readme.md
@@ -0,0 +1,6 @@
+# Print weather
+
+1. Git clone
+2. Build: `gcc main.c -o weather -lcurl -ljson-c`
+3. Run `./weather moscow`
+
--- hw4/main.c
+++ hw4/main.c
@@ -1,29 +1,90 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <json-c/json.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
+typedef struct {
+    char *chars;
+    size_t size;
+} response_chunk;
 
-    char buffer[1024];
-    long http_code;
-    double content_length;
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://wttr.in/Moscow?format=j1");
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK) {
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-            if (http_code == 200) {
-                curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &content_length);
-                snprintf(buffer, sizeof(buffer), "%.0f", content_length);
-                printf("Content-Length: %s\n", buffer);
-            }
-        }
-        curl_easy_cleanup(curl);
+static size_t write_callback(void *data, size_t size, size_t nmemb, void *userp) {
+    size_t chunk_size = size * nmemb;
+    response_chunk *chunk = (response_chunk *) userp;
+    /**
+     * Не могу найти причину почему тут при первом заполнении приходит очень большое число, пробовал уже и обнулять
+     * при первом создании структуры в main и через calloc не вышло... сделал пока такой хак
+     */
+    if (chunk->size > 1000000) {
+        chunk->size = 0;
     }
 
+    char *ptr = realloc(chunk->chars, chunk->size + chunk_size + 1);
+    if (ptr == NULL) {
+        return 0;
+    }
+
+    chunk->chars = ptr;
+    memcpy(&(chunk->chars[chunk->size]), data, chunk_size);
+    chunk->size += chunk_size;
+    chunk->chars[chunk->size] = 0;
+
+    return chunk_size;
+}
+
+int main(int argc, char **argv) {
+    // Validate input args
+    if (argc < 2) {
+        fprintf(stderr, "City wasn't defined, please use: './weather moscow' or './weather Moscow'");
+        exit(EXIT_FAILURE);
+    }
+    char request_url[128];
+    memset(request_url, 0, 128);
+    sprintf(request_url, "https://wttr.in/%s?format=j1", argv[1]);
+
+    // Request preparation
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        fprintf(stderr, "Failed to initialize curl\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Accept: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_URL, request_url);
+
+    response_chunk *chunk = {0};
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
+
+    // Make request
+    CURLcode curlResponse = curl_easy_perform(curl);
+    if (curlResponse != CURLE_OK) {
+        fprintf(stderr, "Curl call error: %s\n", curl_easy_strerror(curlResponse));
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+        exit(EXIT_FAILURE);
+    }
+
+    // Check chunk code
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    if (http_code != 200) {
+        fprintf(stderr, "Error response code from remote server: %ld\n", http_code);
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+        exit(EXIT_FAILURE);
+    }
+
+    /**
+     * Не могу понять почему chunk->chars = "", в дебаге в фунции write_callback значения пишутеся в переменную и она пересоздается
+     * Тут нужна помощь
+     */
+    printf("JSON: %s", chunk->chars);
+    free(chunk->chars);
+
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(curl);
     exit(EXIT_SUCCESS);
 }
